d8fa5a04e17cec0a657b12171d257a197c2c0da1
AdithyaSrivastava01 <adithyasrivastava01@gmail.com>
2024-08-23 23:42:23 -0700

    cahnge in data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+
+# decorator dataclass used to directly define class variables of DataIngestionConfig
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join("artifacts","train.csv")
+    test_data_path: str=os.path.join("artifacts","test.csv")
+    raw_data_path: str=os.path.join("artifacts","raw.csv")
+
+
+# class DataIngestion:
+#     def __init__(self):
+#         self.ingestion = 
